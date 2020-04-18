6eabee27ed0e670eb833fef2baa8f29ce51f3812
Alberto Sierra <alberto.sierra360@outlook.com>
2020-04-18 21:23:45 +0200

    Caso Recursivo

--- PracticaBacktracking/SubcoleccionesDivisor.py
+++ PracticaBacktracking/SubcoleccionesDivisor.py
@@ -1,47 +1,64 @@
 def subcolecciones_principal(coleccion, n, m):
     soluciones = [None] * n
-    subcolecciones(0, coleccion, m, soluciones, 0)
+    s = 0
+    subcolecciones(0, coleccion, n, m, soluciones, s)
+    print(s)
 
 
 # i:            indice de la iteración
 # coleccion:    vector de números
+# m:            tamaño del vector de numeros
 # m:            tamaño de la subcolección
 # soluciones:   vector de boolenos que representa la subcolección
-# s:            número de subcolecciones de tamaño m
-def subcolecciones(i, coleccion, m, soluciones, s):
+# s:            número de subcolecciones de tamaño m que cumplen las restricciones
+def subcolecciones(i, coleccion, n, m, soluciones, s):
     # Caso Base: sub-colección completa
+    # Si la sub-coleccion tiene m elementos y el primero de ellos es el divisor de los demás, he encontrado una solución
+    # Como no es necesario mostrar la solución, simplemente sumo uno a la variable s
     if i == n:
-        divisores = recortar(coleccion, soluciones)
-        if len(divisores) == m and es_divisor(divisores[0], divisores[1:]):
+        divisores = __recortar(coleccion, soluciones)
+        if len(divisores) == m and __es_divisor(divisores[0], divisores[1:]):
             s = s + 1
     else:
-        return 0
+        for k in range(0, 2):
+            # Ramificación en el arbol, se inserta o no el candidato coleccion[i] en la solucion parcial
+            soluciones[i] = k
+
+            # Se expande la solucion
+            subcolecciones(i + 1, coleccion, n, m, soluciones, s)
+
+            # Se deshace el candidato
+            soluciones[i] = None
 
 
 # Devuelve una lista con los elementos de coleccion que estaban a True en solucion
 # El primero de la lista deve ser divisor de los otros m-1 elementos
-def recortar(coleccion, soluciones):
+def __recortar(coleccion, soluciones):
     if soluciones == [] and coleccion == []:
         return []
     else:
-        if soluciones[0] == True:
-            return [coleccion[0]] + recortar(coleccion[1:], soluciones[1:])
+        if soluciones[0]:
+            return [coleccion[0]] + __recortar(coleccion[1:], soluciones[1:])
         else:
-            return recortar(coleccion[1:], soluciones[1:])
+            return __recortar(coleccion[1:], soluciones[1:])
 
 
 # Comprueba si el divisor es divisor de todos los números de la lista dividendos
-def es_divisor(divisor, dividendos):
+def __es_divisor(divisor, dividendos):
     if dividendos == []:
         return True
     else:
-        return (dividendos[0] % divisor == 0) and es_divisor(divisor, dividendos[1:])
+        return (dividendos[0] % divisor == 0) and __es_divisor(divisor, dividendos[1:])
 
 
 n = 5
 coleccion = [2, 2, 5, 4, 7]
-soluciones = [0, 1, 0, 1, 0]
 m = 2
 
+"""
+soluciones = [0, 1, 0, 1, 0]
 divisores = recortar(coleccion, soluciones)
 print(es_divisor(divisores[0], divisores[1:]))
+"""
+
+subcolecciones_principal(coleccion, n, m)
