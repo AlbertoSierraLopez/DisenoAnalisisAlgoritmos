ec058fc3d7acde0e07d964cecd70e64a0bb6c8ea
Alberto Sierra <alberto.sierra360@outlook.com>
2020-05-01 01:59:50 +0200

    Práctica voraces

---
+++ PracticaVoraz/seleccion_actividades.py
@@ -0,0 +1,50 @@
+def seleccion_actividades(n, s, f):
+    _ordenar_actividades(n, s, f)
+    return _algoritmo_voraz(s, f, 0, n)
+
+
+# Ordena los arrays de actividades en función de la finalización (f) de menor a mayor mediante bubble sort. Aunque f
+# guía la ordenación, todos los cambios que se hacen en f deben replicarse en s para mantener la estructura de
+# pares (inicio, fin)
+def _ordenar_actividades(n, s, f):
+    for i in range(n):
+        for j in range(n - 1, i, -1):
+            if f[j] < f[j - 1]:
+                aux_f = f[j]
+                aux_s = s[j]
+                f[j] = f[j - 1]
+                s[j] = s[j - 1]
+                f[j - 1] = aux_f
+                s[j - 1] = aux_s
+
+
+# k representa el subproblema
+def _algoritmo_voraz(s, f, k, n):
+    m = k + 1
+    while m < n and s[m] < f[k]:
+        m = m + 1
+    if m <= n:
+        return 1 + _algoritmo_voraz(s, f, m, n)
+    else:
+        return 0
+
+
+####################
+# Función iterativa para leer una lista de n números enteros
+def lee_lista(n):
+    a = []
+    if n > 0:
+        cadenaEntrada = input()
+        for i in range(0, n):
+            elemento = int(cadenaEntrada.split(" ")[i])
+            a.append(elemento)
+    return a
+
+
+####################
+
+
+n = 11                                      # n = int(input())
+s = [1, 2, 0, 5, 8, 5, 6, 8, 3, 3, 12]      # s = lee_lista(n)
+f = [4, 13, 6, 7, 12, 9, 10, 11, 8, 5, 14]  # f = lee_lista(n)
+print(seleccion_actividades(n, s, f))
