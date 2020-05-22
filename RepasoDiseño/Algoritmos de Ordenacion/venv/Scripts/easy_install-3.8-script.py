8028fd9cb92372e321e72e20e4b0c86989a016dc
Alberto Sierra <alberto.sierra360@outlook.com>
2020-05-22 03:22:32 +0200

    Refactor

--- RepasoDiseño/Algoritmos de Ordenacion/venv/Scripts/easy_install-3.8-script.py
+++ RepasoDiseño/Algoritmos de Ordenacion/venv/Scripts/easy_install-3.8-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\alber\PycharmProjects\AlgoritmosOrdenacion\venv\Scripts\python.exe
+#!C:\Users\alber\PycharmProjects\Algoritmos de Ordenacion\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.8'
 __requires__ = 'setuptools==40.8.0'
 import re
--- RepasoDiseño/Algoritmos de Ordenacion/venv/Scripts/pip3-script.py
+++ RepasoDiseño/Algoritmos de Ordenacion/venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\alber\PycharmProjects\AlgoritmosOrdenacion\venv\Scripts\python.exe
+#!C:\Users\alber\PycharmProjects\Algoritmos de Ordenacion\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
