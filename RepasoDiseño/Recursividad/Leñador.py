a84089554a9f03339da278a31d85d5e23d9e3458
Alberto Sierra <alberto.sierra360@outlook.com>
2020-05-22 18:36:57 +0200

    Refactor

---
+++ RepasoDiseño/Recursividad/Leñador.py
@@ -0,0 +1,21 @@
+def Woodcutter(t, w, h):
+    madera = 0
+    for i in t:
+        if i > h:
+            madera += i - h
+    if madera >= w:
+        return h
+    else:
+        return Woodcutter(t, w, h - 1)
+
+
+def Woodcutter_wrapper(t, w):
+    h = max(t) - 1
+    return Woodcutter(t, w, h)
+
+
+t = [5, 4, 3, 12, 8, 7, 5, 10, 7, 8, 4, 4, 11, 8, 7, 1, 9, 4, 3, 5]
+print("Arboles:", t)
+print("Madera:", end=' ')
+w = int(input())
+print("Altura:", Woodcutter_wrapper(t, w))
