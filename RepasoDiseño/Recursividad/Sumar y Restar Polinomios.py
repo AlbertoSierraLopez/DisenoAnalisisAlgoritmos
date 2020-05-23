31213b101ac87ea6227bb80fdff3920a1080971c
Alberto Sierra <alberto.sierra360@outlook.com>
2020-05-23 03:34:31 +0200

    Polinomios

--- RepasoDiseño/Recursividad/Sumar y Restar Polinomios.py
+++ RepasoDiseño/Recursividad/Sumar y Restar Polinomios.py
@@ -1,5 +1,23 @@
 def Sumar_Polinomios(p, q):
+    gp = len(p)
+    gq = len(q)
+    if gp == 0:
+        return q
+    elif gq == 0:
+        return p
+    else:
+        return Sumar_Polinomios(p[:gp - 1], q[:gq - 1]) + [p[gp - 1] + q[gq - 1]]
 
+
+def Restar_Polinomios(p, q):
+    gp = len(p)
+    gq = len(q)
+    if gp == 0:
+        return q
+    elif gq == 0:
+        return p
+    else:
+        return Restar_Polinomios(p[:gp - 1], q[:gq - 1]) + [p[gp - 1] - q[gq - 1]]
 
 
 def Imprimir_Polinomio(a, i):
@@ -17,5 +35,11 @@ def Imprimir_Polinomio(a, i):
         Imprimir_Polinomio(a[1:], i - 1)
 
 
-a = [3, 5, 0, 2, 1]
+a = [3, 5, 0, 1]
 Imprimir_Polinomio(a, len(a) - 1)
+print("+")
+b = [2, 1, 4, 9, 1]
+Imprimir_Polinomio(b, len(b) - 1)
+print("__________")
+suma = Sumar_Polinomios(a, b)
+Imprimir_Polinomio(suma, len(suma) - 1)
